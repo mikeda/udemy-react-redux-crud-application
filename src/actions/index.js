1901f7572d3ba3157da96c79b8f731898c3006c7
mikeda <baguzy@gmail.com>
2020-10-03 16:42:57 +0900

    新規作成ボタン

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 export const READ_EVENTS = 'READ_EVENTS'
+export const CREATE_EVENT = 'CREATE_EVENT'
 
 const ROOT_URL = "https://udemy-utils.herokuapp.com/api/v1"
 const QUERYSTRING = "?token=tokenmikeda"
@@ -7,4 +8,9 @@ const QUERYSTRING = "?token=tokenmikeda"
 export const readEvents = () => async (dispatch) => {
   const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`);
   dispatch({ type: READ_EVENTS, response })
+}
+
+export const postEvent = (values) => async (dispatch) => {
+  const response = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`, values);
+  dispatch({ type: CREATE_EVENT, response })
 }
