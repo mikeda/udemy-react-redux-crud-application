a5a8c87deef7940da10b845abda70380fad25370
mikeda <baguzy@gmail.com>
2020-10-04 12:24:03 +0900

    イベント詳細表示

--- src/components/EventsShow.js
+++ src/components/EventsShow.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
 import { Link } from 'react-router-dom'
 
-import { deleteEvent, getEvent } from '../actions'
+import { deleteEvent, getEvent, putEvent } from '../actions'
 
 class EventsShow extends Component {
   constructor(props) {
@@ -35,12 +35,12 @@ class EventsShow extends Component {
   }
 
   async onSubmit(values) {
-    await this.props.postEvent(values)
+    await this.props.putEvent(values)
     this.props.history.push("/")
   }
 
   render() {
-    const { handleSubmit, pristine, submitting } = this.props;
+    const { handleSubmit, pristine, submitting, invalid } = this.props;
 
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
@@ -52,7 +52,7 @@ class EventsShow extends Component {
         </div>
 
         <div>
-          <input type="submit" value="Submit" disabled={pristine || submitting} />
+          <input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
           <Link to="/">Cancel</Link>
           <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
         </div>
@@ -74,7 +74,7 @@ const mapStateToProps = (state, ownProps) => {
   const event = state.events[ownProps.match.params.id]
   return { initialValues: event, event }
 }
-const mapDispatchToProps = ({ deleteEvent, getEvent })
+const mapDispatchToProps = ({ deleteEvent, getEvent, putEvent })
 
 export default connect(mapStateToProps, mapDispatchToProps)(
   reduxForm({ validate, form: 'eventShowForm', enableReinitialize: true})(EventsShow)
